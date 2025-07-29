f2a75d43fb391d953875e576cde7a20d54d90ff5
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-07-29 08:44:11 +0530

    set a face book link and instagram link footer side

--- portfolio/src/components/common/Footer.jsx
+++ portfolio/src/components/common/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
         <div className="social-links">
           <a href="https://github.com/sathirapramudith123" target="_blank" rel="noopener noreferrer">GitHub</a>
           <a href="https://www.linkedin.com/in/sathira-pramudith-805284318/?trk=opento_sprofile_pfeditor" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          <a href="https://twitter.com/sathirapramudith" target="_blank" rel="noopener noreferrer">Facebook</a>
-          <a href="https://www.instagram.com/sathirapramudith/" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="https://web.facebook.com/people/Sathira-Pramudith/61560820063930/" target="_blank" rel="noopener noreferrer">Facebook</a>
+          <a href="https://www.instagram.com/_sathira_pramudith_/" target="_blank" rel="noopener noreferrer">Instagram</a>
         </div>
       </div>
     </footer>
