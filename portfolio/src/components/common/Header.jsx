b5ffd34964ea21ba28dcb578388a8042e115bebb
Sathira Pramudith <sathirapramudith1@gmail.com>
2025-07-30 08:54:48 +0530

    responsive a site

--- portfolio/src/components/common/Header.jsx
+++ portfolio/src/components/common/Header.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
 import { useTheme } from '../../context/ThemeContext';
+import { useState } from 'react';
 import ThemeToggle from './ThemeToggle';
 import './Header.css';
 
 const Header = () => {
   const { theme } = useTheme();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
   return (
     <header className={`header ${theme}`}>
@@ -12,14 +16,20 @@ const Header = () => {
         <Link to="/" className="logo">
           Sathira Pramudith
         </Link>
-        <nav className="nav">
+
+        <div className="menu-toggle" onClick={toggleMenu}>
+          &#9776;
+        </div>
+
+        <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
           <ul>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/skills">Skills</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/about" onClick={() => setIsMenuOpen(false)}>About</Link></li>
+            <li><Link to="/skills" onClick={() => setIsMenuOpen(false)}>Skills</Link></li>
+            <li><Link to="/projects" onClick={() => setIsMenuOpen(false)}>Projects</Link></li>
+            <li><Link to="/contact" onClick={() => setIsMenuOpen(false)}>Contact</Link></li>
           </ul>
         </nav>
+
         <ThemeToggle />
       </div>
     </header>
